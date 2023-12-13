c9e5819e92c3abf2415273386ab02fb1eb3e6da9
kvn-cb <invoices.kvn@gmail.com>
2023-12-13 12:53:24 -0600

    Update banner_prin_gre_sto.tsx

--- src/components/banner_prin_gre_sto.tsx
+++ src/components/banner_prin_gre_sto.tsx
@@ -4,8 +4,8 @@ export const BannerPrincipal = () => {
     <div className="hero-overlay bg-opacity-10"></div>
     <div className="hero-content text-center text-neutral-content">
       <div className="max-w-md">
-        <h1 className="mb-5 text-7xl font-bold text-font-nav sm:text-4xl"style={{fontFamily:'Dancing Script,cursive'}}>Bienvenidos a Grema Store</h1>
-        <p className="mb-5 text-2xl text-font-nav">Haz que tus recuerdos duren para siempre con nuestra bisuteria elegante y hermosa.</p>
+        <h1 className="mb-5 text-5xl font-bold text-font-nav sm:text-7xl"style={{fontFamily:'Dancing Script,cursive'}}>Bienvenidos a Grema Store</h1>
+        <p className="mb-5 text-2xl text-font-nav sm:text-3xl">Haz que tus recuerdos duren para siempre con nuestra bisuteria elegante y hermosa.</p>
         <button className="btn bg-nav border-nav">Ir a la tienda</button>
       </div>
     </div>
