3749a84fdc8296fd29629437bbf903f8aaecc5ef
kvn-cb <invoices.kvn@gmail.com>
2023-12-13 12:39:28 -0600

    Update banner_prin_gre_sto.tsx

--- src/components/banner_prin_gre_sto.tsx
+++ src/components/banner_prin_gre_sto.tsx
@@ -1,6 +1,6 @@
 export const BannerPrincipal = () => {
   return (
-    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://grema-store-frontend-code-brains-projects.vercel.app/src/assets/stock-photo-jewelry-making-flatlay-with-semi-precious-stone-beads-and-tools-handmade-jewelry-small-business-2209254561-min.jpg'}}>
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://grema-store-frontend.vercel.app/images/stock-photo-jewelry-making-flatlay-with-semi-precious-stone-beads-and-tools-handmade-jewelry-small-business-2209254561-min.jpg'}}>
     <div className="hero-overlay bg-opacity-10"></div>
     <div className="hero-content text-center text-neutral-content">
       <div className="max-w-md">
