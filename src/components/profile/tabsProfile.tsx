ec420d4b1036d0d9c6a1960944903427627bee2c
kvn-cb <invoices.kvn@gmail.com>
2024-02-28 02:33:59 -0600

    hanlde tabs

--- src/components/profile/tabsProfile.tsx
+++ src/components/profile/tabsProfile.tsx
@@ -1,8 +1,48 @@
+// import { useEffect } from "react";
+import { useState } from "react";
 import { Buttons_Profile } from "./buttons_profile_gre_sto";
 import { Orders } from "./orders_user_gre_sto";
 import { UserCard } from "./user_card_gre_sto";
 
 export const TabsProfile = () => {
+  // useEffect(() => {
+  //   const elementTabs = document.querySelector("#perfil") as HTMLInputElement;
+  //   elementTabs.checked = true;
+  // }, []);
+
+  const [enableTab1, setEnableTab1] = useState(true)
+
+  const handleTabs = (tab: number) => {
+    
+    let elementTabs2;
+    let elementTabs3;
+    let elementTabs4;
+    switch (tab) {
+      case 1:
+        setEnableTab1(true)
+        
+        break;
+      case 2:
+        setEnableTab1(false)
+        elementTabs2 = document.querySelector("#ordenes") as HTMLInputElement;
+        elementTabs2.checked = true;
+        break;
+      case 3:
+        setEnableTab1(false)
+        elementTabs3 = document.querySelector("#carrito") as HTMLInputElement;
+        elementTabs3.checked = true;
+        break;
+      case 4:
+        setEnableTab1(false)
+        elementTabs4 = document.querySelector("#favoritos") as HTMLInputElement;
+        elementTabs4.checked = true;
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className=" flex  justify-center mt-24 ">
       <div className="w-3/4">
@@ -12,9 +52,12 @@ export const TabsProfile = () => {
             name="my_tabs_2"
             role="tab"
             className="tab"
-            aria-label="Perfil de Usuario"
-            id="Perfil de Usuario"
-            
+            aria-label="Perfil"
+            id="#perfil"
+            checked = {enableTab1}
+            onClick={() => {
+              handleTabs(1);
+            }}
           />
           <div
             role="tabpanel"
@@ -22,7 +65,7 @@ export const TabsProfile = () => {
           >
             <div className=" inline-grid justify-center xl:flex">
               <UserCard></UserCard>
-              <Buttons_Profile option = "information"  tab = "1"  ></Buttons_Profile>
+              <Buttons_Profile option="information" tab="1"></Buttons_Profile>
             </div>
           </div>
 
@@ -31,15 +74,18 @@ export const TabsProfile = () => {
             name="my_tabs_2"
             role="tab"
             className="tab"
-            aria-label="Ordenes de compras"
-            id= "Ordenes de compras"
+            aria-label="Ordenes"
+            id="#ordenes"
+            onClick={() => {
+              handleTabs(2);
+            }}
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
             <Orders></Orders>
-            <Buttons_Profile option = ""  tab = "2"  ></Buttons_Profile>
+            <Buttons_Profile option="" tab="2"></Buttons_Profile>
           </div>
 
           <input
@@ -47,15 +93,18 @@ export const TabsProfile = () => {
             name="my_tabs_2"
             role="tab"
             className="tab"
-            aria-label="Carrito de compras"
-            id="Carrito de compras"
+            aria-label="Carrito"
+            id="@carrito"
+            onClick={() => {
+              handleTabs(3);
+            }}
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
-              <Orders></Orders>
-              <Buttons_Profile option = ""  tab = "3"  ></Buttons_Profile>
+            <Orders></Orders>
+            <Buttons_Profile option="" tab="3"></Buttons_Profile>
           </div>
           <input
             type="radio"
@@ -63,16 +112,18 @@ export const TabsProfile = () => {
             role="tab"
             className="tab"
             aria-label="Favoritos"
-            id="Favoritos"
+            id="#favoritos"
+            onClick={() => {
+              handleTabs(4);
+            }}
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
-              <Orders></Orders>
-              <Buttons_Profile option = ""  tab = "4"  ></Buttons_Profile>
+            <Orders></Orders>
+            <Buttons_Profile option="" tab="4"></Buttons_Profile>
           </div>
-
         </div>
       </div>
     </div>
