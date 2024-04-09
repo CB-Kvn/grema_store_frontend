1fb716f1887621fa2719cbafceeb542f6f8f1a5c
kvn-cb <invoices.kvn@gmail.com>
2024-04-09 02:13:01 -0600

    Update categories_products_gre_sto.tsx

--- src/components/store/categories_products_gre_sto.tsx
+++ src/components/store/categories_products_gre_sto.tsx
@@ -1,32 +1,32 @@
 export const Categories = () => {
   return (
     <>
-      <div className="mt-10  grid grid-cols-2 justify-items-center gap-x-4 gap-y-10 sm:grid-cols-2 sm:gap-x-1 md:grid-cols-4 md:gap-x-3  lg:grid-cols-5  lg:gap-x-2  xl:gap-x-3 xl:grid-cols-5">
+      <div className="mt-10  grid grid-cols-2  gap-x-1 gap-y-1 sm:grid-cols-4 sm:gap-x-1 md:grid-cols-4 md:gap-x-3  lg:grid-cols-5  lg:gap-x-2  xl:gap-x-3 xl:grid-cols-5">
         <div className="">
           <img
             src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (1).gif"
-            className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md  rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-36 h-20  sm:w-64 sm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md  rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
         <div className="">
           <img
             src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (2).gif"
-            className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-36 h-20  sm:w-64 sm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
         <div className="">
           <img
             src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (2) (1).gif"
-            className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-36 h-20  sm:w-64 sm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
         <div className="">
           <img
             src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (3).gif"
-            className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32   shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-36 h-20  sm:w-64 sm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32   shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
@@ -34,7 +34,7 @@ export const Categories = () => {
         <div className="md:hidden sm:hidden  hidden">
           <img
             src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (4).gif"
-            className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32   shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-36 h-20  sm:w-64 sm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32   shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
@@ -49,12 +49,12 @@ export const Categories = () => {
       </div>
 
       
-      <div className="mt-10  grid grid-cols-1 justify-items-center gap-x-4 gap-y-10 sm:grid-cols-1 sm:gap-x-1 md:grid-cols-1 md:gap-x-3  lg:grid-cols-5  lg:gap-x-2  xl:gap-x-3 xl:grid-cols-5">
+      <div className="mt-4  grid grid-cols-1 justify-items-center gap-x-1 gap-y-1 sm:grid-cols-1 sm:gap-x-1 md:grid-cols-1 md:gap-x-3  lg:grid-cols-5  lg:gap-x-2  xl:gap-x-3 xl:grid-cols-5">
       
         <div className="">
           <img
             src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (4).gif"
-            className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32    shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-36 h-20 block  lg:hidden xl:hidden sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32    shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
