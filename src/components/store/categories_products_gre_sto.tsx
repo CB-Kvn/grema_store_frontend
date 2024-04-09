d139e2610a7741a6d9fa936493832957321c6a9b
kvn-cb <invoices.kvn@gmail.com>
2024-04-09 01:47:29 -0600

    Update categories_products_gre_sto.tsx

--- src/components/store/categories_products_gre_sto.tsx
+++ src/components/store/categories_products_gre_sto.tsx
@@ -4,28 +4,28 @@ export const Categories = () => {
       <div className="mt-10  grid grid-cols-2 justify-items-center gap-x-4 gap-y-10 sm:grid-cols-2 sm:gap-x-1 md:grid-cols-4 md:gap-x-3  lg:grid-cols-5  lg:gap-x-2  xl:gap-x-3 xl:grid-cols-5">
         <div className="">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (1).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (1).gif"
             className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md  rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
         <div className="">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (2).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (2).gif"
             className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
         <div className="">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (2) (1).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (2) (1).gif"
             className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32  shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
         </div>
         <div className="">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (3).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (3).gif"
             className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32   shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
@@ -33,7 +33,7 @@ export const Categories = () => {
       
         <div className="md:hidden sm:hidden  hidden">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (4).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (4).gif"
             className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32   shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
@@ -41,7 +41,7 @@ export const Categories = () => {
 
         <div className="">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (4).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (4).gif"
             className="w-36 h-20 hidden lg:block xl:block sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32    shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
@@ -53,7 +53,7 @@ export const Categories = () => {
       
         <div className="">
           <img
-            src="/src/assets/Historia frase emprendedores Degradado Rosa (4).gif"
+            src="https://grema-store-frontend.vercel.app/images/Historia frase emprendedores Degradado Rosa (4).gif"
             className="w-36 h-20  sm:w-64 ssm:h-32 md:w-64 md:h-32 lg:w-64 lg:h-32 xl:w-64 xl:h-32    shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
             style={{ border: "solid 2px", borderColor: "#C2A78D" }}
           />
